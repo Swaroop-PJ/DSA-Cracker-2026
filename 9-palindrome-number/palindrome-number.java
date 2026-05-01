25cc97f286543377fedece7345b9fcf02b590507
Swaroop P.J. <jyothiswaroop1611@gmail.com>
2026-05-01 11:15:32 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (34.48%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -29,16 +29,15 @@ class Solution {
 
     //     }
     //     return true;
-    if(x<0) return false;
-    int tmp=x;
-    int rev=0;
-    while(x>0){
-        int rem=x%10;
-        rev=rev*10+rem;
+    if(x<0||x%10==0&&x!=0) return false;
+    int reversedHalf=0;
+    while(x>reversedHalf){
+        reversedHalf=reversedHalf*10+(x%10);
         x/=10;
     }
-    if(rev==tmp) return true;
-    return false;
-}   
+    return (x==reversedHalf||x==reversedHalf/10);
+
+    
+ }
 
 }
