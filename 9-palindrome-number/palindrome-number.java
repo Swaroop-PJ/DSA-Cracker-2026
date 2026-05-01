ddd45cf8d79b745b488202a9e978722d74cf59e8
Swaroop P.J. <jyothiswaroop1611@gmail.com>
2026-05-01 10:50:20 +0530

    Time: 5 ms (79.64%) | Memory: 46.1 MB (34.48%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,34 @@
+class Solution {
+    int digits(int x){
+       int digits=0;
+        while(x>0){
+            digits++;
+            x/=10;
+
+        }
+        return digits;
+    }
+    int expo(int a, int b){
+        int result=1;
+        for(int i=1;i<=b;i++){
+            result*=a;
+        }
+        return result;
+    }
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int digits=digits(x);
+        while(digits>1){
+            int first=x%(10);
+            int last=x/expo(10,digits-1);
+            if(first!=last) return false;
+            x=x/10;
+            digits--;
+            x=x%expo(10,digits-1);
+            digits--;
+
+        }
+        return true;
+        
+    }
+}
