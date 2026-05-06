900015b51f7dd68869945dbe13689ecd32054a97
Swaroop P.J. <jyothiswaroop1611@gmail.com>
2026-05-06 15:22:23 +0530

    Time: 7 ms (93.03%) | Memory: 124 MB (95.19%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,35 +1,32 @@
 class Solution {
-    char[][] rotateClockwise(char [][]Matrix){
-        char [][]T_matrix=new char[Matrix[0].length][Matrix.length];
-        for(int i=0;i<Matrix.length;i++){
-            for(int j=0;j<Matrix[0].length;j++){
-                T_matrix[j][i]=Matrix[i][j];
-            }
-        }
-        for(int i=0;i<T_matrix.length;i++){
-            for(int j=0;j<T_matrix[0].length/2;j++){
-                char tmp=T_matrix[i][j];
-                T_matrix[i][j]=T_matrix[i][T_matrix[0].length-j-1];
-                T_matrix[i][T_matrix[0].length-j-1]=tmp;
-            }
-        }
-        return T_matrix;
-    }
     public char[][] rotateTheBox(char[][] boxGrid) {
-        char T[][]=rotateClockwise(boxGrid);
-        for(int j=0;j<T[0].length;j++){
-            int i,emptyslot;
-            i=emptyslot=T.length-1;
-            for(;i>=0;i--){
-                if(T[i][j]=='*') emptyslot=i-1;
-                else if(T[i][j]=='#'){
-                    char tmp= T[emptyslot][j];
-                    T[emptyslot][j]=T[i][j];
-                    T[i][j]=tmp;
-                    emptyslot--;
+        int m=boxGrid.length;
+        int n=boxGrid[0].length;
+        char matrix[][]=new char[n][m];
+        for(int i=0;i<m;i++){
+            int emptySlot=n-1;
+            for(int j=n-1;j>=0;j--){
+                int targetColumn=m-i-1;
+                if(boxGrid[i][j]=='*'){
+                    matrix[j][targetColumn]='*';
+
+                    while(emptySlot>j){
+                        matrix[emptySlot][targetColumn]='.';
+                        emptySlot--;
+                    }
+                    emptySlot--;
+                }
+                else if(boxGrid[i][j]=='#'){
+                    matrix[emptySlot][targetColumn]='#';
+                    emptySlot--;
                 }
             }
+            while(emptySlot>=0){
+                matrix[emptySlot][m-i-1]='.';
+                emptySlot--;
+            }
         }
-        return T;
+        return matrix;
+        
     }
 }
