8ac3bd6e7ea4a558b39f46a7592121e9923233c0
Swaroop P.J. <jyothiswaroop1611@gmail.com>
2026-05-07 01:48:51 +0530

    Time: 2 ms (91.70%) | Memory: 47.8 MB (37.63%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int length=nums.length;
+        int j=0;
+        for(int i=0;i<length;i++){
+            if(nums[i]!=0){
+                if(i!=j){
+                nums[j]=nums[i];
+                nums[i]=0;
+                }
+                j++; 
+            }
+        }
+        return;
+    }
+}
